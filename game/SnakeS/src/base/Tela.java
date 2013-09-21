32a40463c94287ae676860000dfa486694daccea
lucasbegnini <lucasbegnini@gmail.com>
2013-09-21 19:25:15 -0400

    [T3]

--- game/SnakeS/src/base/Tela.java
+++ game/SnakeS/src/base/Tela.java
@@ -1,7 +1,11 @@
 package base;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
@@ -10,6 +14,18 @@ public class Tela extends JPanel {
 	//Variáveis de Instância
 	private int alturaY;
 	private int larguraX;
+	private int TPONTOS = 768;
+	private int PONTO = 25;
+    // Definição do plano cartesiano (x,y) do jogo
+    private int[] x = new int[TPONTOS];
+    private int[] y = new int[TPONTOS];
+    
+    // Posição (x,y) da comida
+    private int comidax;
+    private int comiday;
+    
+    //Imagem da comida
+    private Image comida; 
 	
 	public int getAlturaY() {
 		return alturaY;
@@ -29,7 +45,47 @@ public class Tela extends JPanel {
 
 	public void init() {
 		
+ 
+		
+        posicaoComida();
+        
+        
 	}
+	// Método para gera posicao comida
+	public void posicaoComida()
+	{
+    	//Cordenadas da comida
+    	int comidax = larguraX/2;
+    	int comiday = alturaY/2;
+	}
+	
+	// Metodo para abrir as imagens
+	public void abriImagens()
+	{
+        // Cria um icone do arquivo png e seta na imagem correspondente
+        ImageIcon comida_ = new ImageIcon("comida.png");
+        comida = comida_.getImage();
+	}
+	
+	
+	
+	// Método para desenhar elementos na tela do jogo
+    @Override
+    public void paint (Graphics g)
+    {
+        // Define o atribuito para a classe própria
+        super.paint(g);
+
+        // Desenha a comida no plano (x,y) do jogo
+    
+        g.drawImage(comida, comidax, comiday, this);
+    
+        // Executa a sincronia de dados
+        Toolkit.getDefaultToolkit().sync();
+
+        // Pausa os gráficos
+        g.dispose();
+    }
 	
 	
 	/**
@@ -49,7 +105,12 @@ public class Tela extends JPanel {
 		//Define tamanho do JPanel
 		setSize(this.getLarguraX(), this.getAlturaY());
 		
+		abriImagens();
+		
+		init();
 
-				
+
 	}
+	
+
 }
