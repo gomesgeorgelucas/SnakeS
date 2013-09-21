81a6c30d9d7164afb75b92987fe451359c0fa680
George Lucas <gomes.georgelucas@gmail.com>
2013-09-21 17:23:19 -0400

    T1 OK

--- game/SnakeS/src/base/Main.java
+++ game/SnakeS/src/base/Main.java
@@ -1,9 +1,36 @@
 package base;
 
-//
-public class Main {
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Main extends JFrame{
+		
+	public Main() {
+		
+		//Instancia JPanel
+		Tela telaJogo = new Tela();
+		
+		//Associa um JPanel a um JFrame
+		this.add(telaJogo);
+		
+		//Define tamanho do JFrame
+		//Deve ser maior que o JPanel
+		this.setSize(telaJogo.getLarguraX()+20,
+				telaJogo.getAlturaY()+40);
+		
+		//Define redimensionamento
+		this.setResizable(false);
+		
+		//Define título do JFrame
+		this.setTitle("SnakeS");
+		
+	}
+	
+	
 	
 	public static void main(String args[]) {
+		new Main().setVisible(true);
+		
 		
 	}
 }
--- game/SnakeS/src/base/Tela.java
+++ game/SnakeS/src/base/Tela.java
@@ -1,9 +1,55 @@
 package base;
 
-//
-public class Tela {
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Tela extends JPanel {
 	
-	public Tela() {
+	//Variáveis de Instância
+	private int alturaY;
+	private int larguraX;
+	
+	public int getAlturaY() {
+		return alturaY;
+	}
+
+	public void setAlturaY(int alturaY) {
+		this.alturaY = alturaY;
+	}
+
+	public int getLarguraX() {
+		return larguraX;
+	}
+
+	public void setLarguraX(int larguraX) {
+		this.larguraX = larguraX;
+	}
+
+	public void init() {
 		
+	}
+	
+	
+	/**
+	 * Construtor da Tela
+	 */
+	public Tela() {
+		//Define área útil da tela em pixels
+		this.setLarguraX(800);
+		this.setAlturaY(600);
+				
+		//Define cor fixa do fundo
+		setBackground(Color.WHITE);
+		
+		//Foca no painel do jogo ao abrir
+		setFocusable(true);
+		
+		//Define tamanho do JPanel
+		setSize(this.getLarguraX(), this.getAlturaY());
+		
+
+				
 	}
 }
