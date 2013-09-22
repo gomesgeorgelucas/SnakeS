2258c37c4a3ca3a5d72b209971dec403eb8390f5
Giovanni-RC <giovanni.rcaldeira@gmail.com>
2013-09-21 20:26:13 -0400

    [T3] OK

--- game/SnakeS/src/base/Tela.java
+++ game/SnakeS/src/base/Tela.java
@@ -14,19 +14,29 @@ public class Tela extends JPanel {
 	//Variáveis de Instância
 	private int alturaY;
 	private int larguraX;
-	private int TPONTOS = 768;
-	private int PONTO = 25;
-    // Definição do plano cartesiano (x,y) do jogo
-    private int[] x = new int[TPONTOS];
-    private int[] y = new int[TPONTOS];
+	private int totalPontos = 768;
+	private int tamanhoPonto = 25;
     
-    // Posição (x,y) da comida
-    private int comidax;
-    private int comiday;
+	
+    private int[] pontosEmX = new int[totalPontos];
+    private int[] pontoEmY = new int[totalPontos];
+    
+
+
+    private int frutaX;
+    private int frutaY;
     
     //Imagem da comida
-    private Image comida; 
+    private Image fruta; 
 	
+	public Image getFruta() {
+		return fruta;
+	}
+
+	public void setFruta(Image fruta) {
+		this.fruta = fruta;
+	}
+
 	public int getAlturaY() {
 		return alturaY;
 	}
@@ -42,48 +52,99 @@ public class Tela extends JPanel {
 	public void setLarguraX(int larguraX) {
 		this.larguraX = larguraX;
 	}
+	
+	public int getTotalPontos() {
+		return totalPontos;
+	}
 
+	public void setTotalPontos(int totalPontos) {
+		this.totalPontos = totalPontos;
+	}
+
+	public int getTamanhoPonto() {
+		return tamanhoPonto;
+	}
+
+	public void setTamanhoPonto(int tamanhoPonto) {
+		this.tamanhoPonto = tamanhoPonto;
+	}
+
+	public int[] getPontosEmX() {
+		return pontosEmX;
+	}
+
+	public void setPontosEmX(int[] pontosEmX) {
+		this.pontosEmX = pontosEmX;
+	}
+
+	public int[] getPontoEmY() {
+		return pontoEmY;
+	}
+
+	public void setPontoEmY(int[] pontoEmY) {
+		this.pontoEmY = pontoEmY;
+	}
+
+	public int getFrutaX() {
+		return frutaX;
+	}
+
+	public void setFrutaX(int frutaX) {
+		this.frutaX = frutaX;
+	}
+
+	public int getFrutaY() {
+		return frutaY;
+	}
+
+	public void setFrutaY(int frutaY) {
+		this.frutaY = frutaY;
+	}
+
+	/**
+	 * Método para iniciar o jogo
+	 */
+	
 	public void init() {
-		
- 
-		
-        posicaoComida();
-        
+	
+        posicaofruta();
         
 	}
-	// Método para gera posicao comida
-	public void posicaoComida()
+	
+	/**
+	 * Método para gerar a posição da fruta
+	 */
+	public void posicaofruta()
 	{
     	//Cordenadas da comida
-    	int comidax = larguraX/2;
-    	int comiday = alturaY/2;
+    	frutaX = (larguraX/2);
+    	frutaY = (alturaY/2);
 	}
 	
-	// Metodo para abrir as imagens
-	public void abriImagens()
+	/**
+	 * Método para abrir imagens
+	 */
+	public void abrirImagens()
 	{
-        // Cria um icone do arquivo png e seta na imagem correspondente
-        ImageIcon comida_ = new ImageIcon("comida.png");
-        comida = comida_.getImage();
+        ImageIcon frutaIcone = new ImageIcon("fruta.png");
+        fruta = frutaIcone.getImage();
 	}
 	
 	
 	
-	// Método para desenhar elementos na tela do jogo
+	/**
+	 * Método para atualizar o desenho da tela
+	 */
     @Override
     public void paint (Graphics g)
     {
-        // Define o atribuito para a classe própria
+       
         super.paint(g);
-
-        // Desenha a comida no plano (x,y) do jogo
     
-        g.drawImage(comida, comidax, comiday, this);
-    
-        // Executa a sincronia de dados
+        g.drawImage(fruta, frutaX, frutaY, this);
+            
         Toolkit.getDefaultToolkit().sync();
 
-        // Pausa os gráficos
         g.dispose();
     }
 	
@@ -92,6 +153,7 @@ public class Tela extends JPanel {
 	 * Construtor da Tela
 	 */
 	public Tela() {
+		
 		//Define área útil da tela em pixels
 		this.setLarguraX(800);
 		this.setAlturaY(600);
@@ -105,7 +167,7 @@ public class Tela extends JPanel {
 		//Define tamanho do JPanel
 		setSize(this.getLarguraX(), this.getAlturaY());
 		
-		abriImagens();
+		abrirImagens();
 		
 		init();
 
